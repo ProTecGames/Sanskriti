4e295518f27aebcbe5f39c72bb73422dd083d41e
Prakhar Doneria <gamesprotec@gmail.com>
2023-12-19 14:55:37 +0530

    Hit and Try Fix

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
-// script.js
-
-// Dictionary to store variables
 const variables = {};
 
-// New function to find the length of a string, list, or tuple
 function ayamah(data) {
   if (typeof data === 'string' || Array.isArray(data) || data instanceof Set || data instanceof Map) {
     return data.length;
@@ -12,25 +8,21 @@ function ayamah(data) {
   }
 }
 
-// New function to add as many numbers as entered
 function kulam() {
   const numbers = Array.from(arguments);
   return numbers.reduce((acc, curr) => acc + curr, 0);
 }
 
-// New function to subtract as many numbers as entered
 function ghatah() {
   const numbers = Array.from(arguments);
   return numbers.reduce((acc, curr) => acc - curr);
 }
 
-// New function to multiply as many numbers as entered
 function gunanam() {
   const numbers = Array.from(arguments);
   return numbers.reduce((acc, curr) => acc * curr, 1);
 }
 
-// New function to divide as many numbers as entered
 function bhajah() {
   const numbers = Array.from(arguments);
   return numbers.reduce((acc, curr) => acc / curr);
@@ -45,18 +37,15 @@ function runCode() {
     let output = '';
 
     lines.forEach(line => {
-      // Ignore lines starting with #
       if (line.trim().startsWith('#')) {
         return;
       }
 
-      // Variable declaration syntax with input
       if (line.trim().includes('=')) {
         const [variable, value] = line.split('=').map(part => part.trim());
         variables[variable] = evaluateValue(value);
       }
 
-      // If-else statement syntax
       if (line.trim().startsWith('yadi')) {
         const condition = line.match(/yadi (.+):/)[1];
         const blockStartIndex = lines.indexOf(line) + 1;
@@ -69,7 +58,6 @@ function runCode() {
               const data = ifLine.match(/mudranam\((.*)\)/)[1];
               output += mudranam(evaluateValue(data));
             }
-            // Add more logic for other Sanskrit commands inside if block
           });
         } else {
           const elseBlock = lines.slice(blockEndIndex + 1);
@@ -78,31 +66,24 @@ function runCode() {
               const data = elseLine.match(/mudranam\((.*)\)/)[1];
               output += mudranam(evaluateValue(data));
             }
-            // Add more logic for other Sanskrit commands inside else block
           });
         }
       }
 
-      // mudranam() function syntax
       if (line.trim().startsWith('mudranam(')) {
         const data = line.match(/mudranam\((.*)\)/)[1];
         output += mudranam(evaluateValue(data));
       }
 
-      // New function calls
       if (line.trim().startsWith('ayamah(') || line.trim().startsWith('kulam(') ||
           line.trim().startsWith('ghatah(') || line.trim().startsWith('gunanam(') ||
           line.trim().startsWith('bhajah(')) {
         const data = line.match(/\((.*)\)/)[1];
         output += mudranam(evaluateValue(`${line.trim().split('(')[0]}(${data})`));
       }
-      // Add more logic for other Sanskrit commands as needed
     });
 
-    // Display the final output
     outputDiv.innerHTML = output;
-
-    // Switch to the "Output" tab after running the code
     switchTab('output-tab', 'Output');
 
   } catch (error) {
@@ -111,33 +92,27 @@ function runCode() {
 }
 
 function mudranam(data) {
-  return data;
+  console.log(data); // Modify this line to display the output
 }
 
 function evaluateValue(value) {
-  // Evaluate the value if it's a variable or an expression
   if (variables.hasOwnProperty(value)) {
     return variables[value];
   } else if (value.startsWith('{') && value.endsWith('}')) {
-    // Parse and return dictionary
     return JSON.parse(value.replace(/'/g, '"'));
   } else if (value.startsWith('[') && value.endsWith(']')) {
-    // Parse and return list
     return JSON.parse(value.replace(/'/g, '"'));
   } else if (value.startsWith('(') && value.endsWith(')')) {
-    // Parse and return tuple
     return value.replace(/[()]/g, '').split(',').map(item => item.trim());
   } else if (value.startsWith('"') && value.endsWith('"')) {
-    return value.substring(1, value.length - 1); // Print data as a string
+    return value.substring(1, value.length - 1);
   } else if (value.startsWith('abhivinmuln("') && value.endsWith('")')) {
-    // Input string
     return prompt(value.substring(14, value.length - 2));
   } else if (value.startsWith('purnanka(abhivinmuln("') && value.endsWith('"))')) {
-    // Input integer
     const input = prompt(value.substring(21, value.length - 3));
     return parseInt(input);
   } else {
-    return eval(value); // Try printing data from variable
+    return eval(value);
   }
 }
 
@@ -164,18 +139,14 @@ function findBlockEndIndex(lines, startIndex) {
 }
 
 function switchTab(tabId, section) {
-  // Remove 'active' class from all tabs
   document.getElementById('editor-tab').classList.remove('active');
   document.getElementById('output-tab').classList.remove('active');
 
-  // Add 'active' class to the clicked tab
   document.getElementById(tabId).classList.add('active');
 
-  // Toggle visibility of the corresponding section
   document.getElementById('code').classList.toggle('hidden', section !== 'Editor');
   document.getElementById('output').classList.toggle('hidden', section !== 'Output');
 }
 
-// Initial code to handle input and line numbers
 document.getElementById('code').addEventListener('input', handleInput);
 handleInput();
