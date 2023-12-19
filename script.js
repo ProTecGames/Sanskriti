f3467e260a5c71aaeeec3986b74882e415b62421
Prakhar Doneria <gamesprotec@gmail.com>
2023-12-19 11:09:25 +0530

    Test 1

--- script.js
+++ script.js
@@ -14,35 +14,32 @@ function switchTab(tabId, section) {
 }
 
 function runCode() {
-  // Your code execution logic goes here
   const code = document.getElementById('code').value;
   const outputDiv = document.getElementById('output');
   
   try {
-    // Add Sanskrit code interpretation and execution logic here
-
-    // Split lines and execute each line
     const lines = code.split('\n');
+    let output = '';
+
     lines.forEach(line => {
       if (line.trim().startsWith('mudranam(')) {
         const data = line.match(/mudranam\((.*)\)/)[1];
-        print(eval(data)); // Using eval for simplicity (consider other approaches in a real implementation)
+        output += mudranam(eval(data)); // Using eval for simplicity (consider other approaches in a real implementation)
       }
       // Add more logic for other Sanskrit commands as needed
     });
 
+    // Display the final output
+    outputDiv.innerHTML = output;
+
+    // Switch to the "Output" tab after running the code
+    switchTab('output-tab', 'Output');
+
   } catch (error) {
-    // Display error if code has a syntax error
     outputDiv.innerHTML = `<p style="color: red;">Syntax Error: ${error.message}</p>`;
   }
 }
 
-function print(data) {
-  const outputDiv = document.getElementById('output');
-  outputDiv.innerHTML += '<p>' + data + '</p>';
-}
-
-  // For now, let's just display a message in the output area
-  const outputDiv = document.getElementById('output');
-  outputDiv.innerHTML = '<p>Code executed successfully!</p>';
+function mudranam(data) {
+  return data;
 }
