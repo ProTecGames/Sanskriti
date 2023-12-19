6c296495e789067610ce39938f8aa48324078092
Prakhar Doneria <gamesprotec@gmail.com>
2023-12-19 11:56:04 +0530

    Minor Fix in Tab selector

--- script.js
+++ script.js
@@ -54,3 +54,16 @@ function evaluateValue(value) {
   // Otherwise, return the original value
   return value;
 }
+
+function switchTab(tabId, section) {
+  // Remove 'active' class from all tabs
+  document.getElementById('editor-tab').classList.remove('active');
+  document.getElementById('output-tab').classList.remove('active');
+
+  // Add 'active' class to the clicked tab
+  document.getElementById(tabId).classList.add('active');
+
+  // Toggle visibility of the corresponding section
+  document.getElementById('code').classList.toggle('hidden', section !== 'Editor');
+  document.getElementById('output').classList.toggle('hidden', section !== 'Output');
+}
